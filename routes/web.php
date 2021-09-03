042c19eb9f2f7b239baa3e97f450334692ac31e1
antoniovenezia <antoniovenezia00@gmail.com>
2021-09-03 09:33:38 +0200

    laravel comics

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('base');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about-us', function () {
+    return view('about-us');
 });
